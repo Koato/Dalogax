b5a3690f13c8ee1ba82f5ab6c7699fc1b66f7b2b
Andy Gomez <i201622650@cibertec.edu.pe>
2023-09-27 05:17:06 -0500

    add cors and restTemplate implement

--- src/main/java/com/kalagan/dalogax/services/impl/ProductImpl.java
+++ src/main/java/com/kalagan/dalogax/services/impl/ProductImpl.java
@@ -1,12 +1,18 @@
 package com.kalagan.dalogax.services.impl;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
 import com.kalagan.dalogax.dao.ProductDetail;
 import com.kalagan.dalogax.dao.SimilarProduct;
 import com.kalagan.dalogax.services.IProductService;
@@ -15,32 +21,50 @@ import com.kalagan.dalogax.services.IProductService;
 public class ProductImpl implements IProductService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProductImpl.class);
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<SimilarProduct> getListProductSimilarId(String productId) {
-		StringBuilder url = new StringBuilder("http://localhost:3001/");
-		url.append("product/");
+		StringBuilder url = new StringBuilder("http://localhost:3001/product/");
 		url.append(productId.trim());
 		url.append("/similarids");
 		LOGGER.info("url: {}", url);
-		List<SimilarProduct> resultado = restTemplate.getForObject(url.toString(), List.class);
-		LOGGER.info("resultado: {}", resultado.toString());
-		return resultado;
+		try {
+			ResponseEntity<?> resultado = restTemplate.getForEntity(url.toString(), List.class);
+			LOGGER.info("status: {}", resultado.getStatusCode());
+			if (resultado.getStatusCode() == HttpStatus.OK) {
+				LOGGER.info("resultado: {}", resultado.getBody());
+				return (List<SimilarProduct>) resultado.getBody();
+			}
+		} catch (HttpClientErrorException.NotFound e) {
+			LOGGER.error("El recurso solicitado no fue encontrado.");
+		} catch (Exception e) {
+			LOGGER.error("El recurso no responde segun lo esperado.");
+		}
+		return Collections.EMPTY_LIST;
 	}
 
 	@Override
 	public Optional<ProductDetail> getProductSimilar(String productId) {
-		StringBuilder url = new StringBuilder("http://localhost:3001/");
-		url.append("product/");
+		StringBuilder url = new StringBuilder("http://localhost:3001/product/");
 		url.append(productId.trim());
 		LOGGER.info("url: {}", url);
-		ProductDetail resultado = restTemplate.getForObject(url.toString(), ProductDetail.class);
-		LOGGER.info("resultado: {}", resultado);
-		return Optional.ofNullable(resultado);
+		try {
+			ResponseEntity<?> resultado = restTemplate.getForEntity(url.toString(), ProductDetail.class);
+			LOGGER.info("status: {}", resultado.getStatusCode());
+			if (resultado.getStatusCode() == HttpStatus.OK) {
+				LOGGER.info("resultado: {}", resultado.getBody());
+				return Optional.of((ProductDetail) resultado.getBody());
+			}
+		} catch (HttpClientErrorException.NotFound e) {
+			LOGGER.error("El recurso solicitado no fue encontrado.");
+		} catch (Exception e) {
+			LOGGER.error("El recurso no responde segun lo esperado.");
+		}
+		return Optional.empty();
 	}
 
 }
